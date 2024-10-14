870588b7a64c1e44dcc43e28ac1eb6f37e087784
luisgcode <luisgcode@gmail.com>
2024-10-14 18:41:48 -0400

    Starting with chartjs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">{/* Insert the graph here */}</div>;
+  return (
+    <div className="App">
+      Testing gh page first{/* Insert the graph here */}
+    </div>
+  );
 }
 
 export default App;
